605b6d8b47601417d016654f0b29a9f463802185
amotz taub tabib <amotz.tt@gmail.com>
2019-01-04 15:30:16 +0200

    another change

--- amotz.sort.py
+++ amotz.sort.py
@@ -4,12 +4,17 @@
 
 # TODO(aviv): Automate the testing.
 # I verified that when I break this function, running amotzsort([9, 4, 9, 2]) fails with an assertion error.
+#[amotz] you are right, this function is not relevant and was part f my thinking process, i change the name now for preliminarytest
 def isSorted(array):
     return all(array[i] <= array[i + 1] for i in range(len(array) - 1))
 
 
+
+
+
 # [aviv] I'm not sure what this function does, but it doesn't sort the input correctly.
-def amotzsort(x):
+#[amotz] this function was part of my thought process and is indeed not relevant.
+def preliminarytest(x):
     print(x)
     count = x[0]
     for i in range(0, len(x)):
@@ -37,11 +42,10 @@ def amotzsort(x):
     print(x)
 
 
-amotzsort([3, 1, 1, 3])
-
-
 # [aviv] I'm not sure what this function does. It doesn't return anything, and it isn't used anywhere.
-def findingmax(x):
+# [amotz] this function is suppose to find a maximum of arrays
+# that reduce in size by 1 but it was also part of my thinking proccess and is not relevant, i changed the name to findingmaxtest1
+def findingmaxtest1(x):
     for i1 in range(0, len(x)):
         # [aviv] "count" is not a good name for this variable.
         count = x[i1]
@@ -51,31 +55,35 @@ def findingmax(x):
         print(count)
 
 
-findingmax([15, 81, 32, 78])
 
-
-def sortinglist(x):
-    print("the array is=", x)
+# [amotz] this is the actual function and as you can see below, it produce True for various arrays
+# that are tested with your function isSorted, i changed the name for amotzsort to reduce confusion
+def amotzsort(x):
+    #print("the array is=", x)
     for i1 in range(0, len(x)):
-        print("i1=", i1)
+        #print("i1=", i1)
         # [aviv] "count" is not a good name for this variable.
         count = x[i1]
-        print("count=", count)
+        #print("count=", count)
         for i in range(i1, len(x)):
-            print("i=", i)
-            print("x[i]=", x[i])
+          #  print("i=", i)
+         #   print("x[i]=", x[i])
             if count > x[i]:
                 count = x[i]
-                print("x.index(count,i1,len(x))=", x.index(count, i1, len(x)))
-        print("count=", count)
+        #        print("x.index(count,i1,len(x))=", x.index(count, i1, len(x)))
+        #print("count=", count)
         a = i1
         b = x.index(count, i1, len(x))
-        print("b=", b)
-        print("a=", a)
+        #print("b=", b)
+        #print("a=", a)
         x[a], x[b] = x[b], x[a]
-        print("the array is=", x)
+        #print("the array is=", x)
     assert isSorted(x)
     return (x)
 
 
-print(sortinglist([3, 1, 8, 9, 8, 23, 1, 34, 67, 67, 89, 23]))
+print(isSorted(amotzsort([3, 5, 11, 3, 13])))
+print(isSorted(amotzsort([7, 8, 7, 9, 5, 2])))
+print(isSorted(amotzsort([6, 8, 8, 5, 4, 3])))
+print(isSorted(amotzsort([6, 8, 9, 9, 4, 3])))
+print(isSorted(amotzsort([8, 6, 6, 6, 9, 3, 2, 1, 2])))
