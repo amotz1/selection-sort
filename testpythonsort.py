986ed7c60648a44aedaa605556a6fb79e5084e41
aviv <yotam@aviv.net>
2019-03-09 09:35:43 -0800

    comments

--- testpythonsort.py
+++ testpythonsort.py
@@ -13,13 +13,17 @@ for i in range(NUM_TESTS):
     # [aviv] Either use constants or just test over the entire range of integers.
     y = ([random.randrange(1, 4), random.randrange(1, 4), random.randrange(1, 4)])
     print("the random array to be sorted is", y)
+    # [aviv] You call sorted(..) here for the first time.
     print("the sorted array is", sorted(y))
     start = timeit.default_timer()
+    # [aviv] You now measure sorted, but this is the second call, and so it will be faster due to caching.
     sorted(y)
     time = timeit.default_timer() - start
     array.append(time)
     # [aviv] This is not a correct test, and it would not work when we get to sorting a list of length 1,000,000.
     # I'd just remove it and test correctness correctly somewhere else.
+    # [aviv] You call sorted(..) 3 times here. It's wasteful and bad form. (But I've recommended that you remove this
+    # anyway.)
     if sorted(y)[0] <= sorted(y)[1] <= sorted(y)[2]:
         print("you got it")
 print("the maximum time is", max(array))
